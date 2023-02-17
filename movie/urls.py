48db8f5aeb7553d5d3cd1878057766a3dccaee02
seanf316 <seanfinn316@gmail.com>
2023-02-17 00:36:50 +0000

    Movie details view/template created

--- movie/urls.py
+++ movie/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("trending/<str:url>/", views.trending, name="trending"),
-    path("trending/page/<page_number>/", views.pagination, name="pagination"),
+    path("trending/", views.trending, name="trending"),
+    path(
+        "trending/page/<page_number>/",
+        views.pagination,
+        name="pagination",
+    ),
     path("toprated/", views.toprated, name="toprated"),
+    path(
+        "moviedetails/<int:movie_id>", views.moviedetails, name="moviedetails"
+    ),
 ]
--- movie/views.py
+++ movie/views.py
@@ -6,12 +6,11 @@ import requests
 API_KEY = os.environ.get("API_KEY")
 
 
-def trending(request, url):
+def trending(request):
     """
     Call on the TMDB API to provide some trending movies
     """
-
-    url = f"https://api.themoviedb.org/3/{url}?api_key={API_KEY}&language=en-US&page=1"
+    url = f"https://api.themoviedb.org/3/trending/movie/week?api_key={API_KEY}&language=en-US&page=1"
 
     response = requests.get(url)
     movie_data = response.json()
@@ -42,7 +41,7 @@ def toprated(request):
 
 
 def pagination(request, page_number):
-    url = f"https://api.themoviedb.org/3/trending/movie/day?api_key={API_KEY}&language=en-US&page={str(page_number)}"
+    url = f"https://api.themoviedb.org/3/trending/movie/week?api_key={API_KEY}&language=en-US&page=1{str(page_number)}"
 
     response = requests.get(url)
     movie_data = response.json()
@@ -54,3 +53,18 @@ def pagination(request, page_number):
     }
 
     return render(request, "movie/trending.html", context)
+
+
+def moviedetails(request, movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+
+    response = requests.get(url)
+    movie_data = response.json()
+    backdrop = movie_data["backdrop_path"]
+    hero = "https://image.tmdb.org/t/p/w1280/" + backdrop
+    context = {
+        "movie_data": movie_data,
+        "hero": hero,
+    }
+
+    return render(request, "movie/moviedetails.html", context)
