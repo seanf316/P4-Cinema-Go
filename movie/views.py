f84e10208616876bc34e2649928359d2001a1947
seanf316 <seanfinn316@gmail.com>
2023-02-18 02:05:48 +0000

    More styling applied to Movie Details page

--- movie/views.py
+++ movie/views.py
@@ -56,15 +56,34 @@ def pagination(request, page_number):
 
 
 def moviedetails(request, movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US&append_to_response=credits,videos,images"
 
     response = requests.get(url)
     movie_data = response.json()
     backdrop = movie_data["backdrop_path"]
     hero = "https://image.tmdb.org/t/p/w1280/" + backdrop
+
+    director = None
+    for person in movie_data["credits"]["crew"]:
+        if person["job"] == "Director":
+            director = person
+            break
+
+    director_name = director["name"] if director is not None else None
+
+    trailer = None
+    for video in movie_data["videos"]["results"]:
+        if video["type"] == "Trailer":
+            trailer = video
+            break
+
+    trailer_key = trailer["key"] if trailer is not None else None
+
     context = {
         "movie_data": movie_data,
         "hero": hero,
+        "director_name": director_name,
+        "trailer": trailer_key,
     }
 
     return render(request, "movie/moviedetails.html", context)
