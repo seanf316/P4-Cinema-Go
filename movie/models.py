85ca025f456392503515c29a816660ef6e020124
seanf316 <seanfinn316@gmail.com>
2023-02-21 00:47:57 +0000

    Movie model fields updated with null=True

--- movie/models.py
+++ movie/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Movie(models.Model):
-    Name = models.CharField(max_length=100, blank=True)
-    Overview = models.TextField(blank=True)
-    Director = models.CharField(max_length=50, blank=True)
-    Released = models.CharField(max_length=25, blank=True)
-    Runtime = models.CharField(max_length=25, blank=True)
-    MovieId = models.CharField(max_length=25, blank=True)
+    Name = models.CharField(max_length=100, null=True, blank=True)
+    Overview = models.TextField(null=True, blank=True)
+    Director = models.CharField(max_length=50, null=True, blank=True)
+    Released = models.CharField(max_length=25, null=True, blank=True)
+    Runtime = models.CharField(max_length=25, null=True, blank=True)
+    MovieId = models.CharField(max_length=25, null=True, blank=True)
 
     class Meta:
         ordering = ["Name"]
