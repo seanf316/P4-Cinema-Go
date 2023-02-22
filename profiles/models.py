12be2f0bf6024bea818384279cd5da30be466e9b
seanf316 <seanfinn316@gmail.com>
2023-02-22 23:30:42 +0000

    Changed variables to lowercase

--- profiles/models.py
+++ profiles/models.py
@@ -6,13 +6,13 @@ placeholder = "https://res.cloudinary.com/seanf316/image/upload/v1677023649/defa
 
 
 class Profile(models.Model):
-    User = models.OneToOneField(
+    user = models.OneToOneField(
         User, on_delete=models.CASCADE, blank=False, related_name="profile"
     )
-    First_Name = models.CharField(max_length=50, null=True, blank=True)
-    Surname = models.CharField(max_length=50, null=True, blank=True)
-    About = models.TextField(max_length=200, null=True, blank=True)
-    Profile_Image = CloudinaryField("image", default=placeholder)
+    firstname = models.CharField(max_length=50, null=True, blank=True)
+    surname = models.CharField(max_length=50, null=True, blank=True)
+    about = models.TextField(max_length=200, null=True, blank=True)
+    profile_image = CloudinaryField("image", default=placeholder)
 
     def __str__(self):
-        return str(self.User)
+        return str(self.user)
