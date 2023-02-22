f9589d5a8244c22df774df33dd4038f7621624e3
seanf316 <seanfinn316@gmail.com>
2023-02-22 23:39:28 +0000

    profile view created

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import requests
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your views here.
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+
+    context = {
+        "profile": profile,
+    }
+
+    return render(request, "profiles/profile.html", context)
