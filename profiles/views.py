4a548be299f0d69290c734d099b358d3d7938627
seanf316 <seanfinn316@gmail.com>
2023-02-24 18:50:58 +0000

    edit_profile view created

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 import requests
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import ProfileForm, UserForm
 
 
 def profile(request, username):
@@ -13,3 +15,31 @@ def profile(request, username):
     }
 
     return render(request, "profiles/profile.html", context)
+
+
+def edit_profile(request, username):
+
+    user = get_object_or_404(User, username=username)
+    profile = Profile.objects.get(user=user)
+
+    if request.method == "POST":
+        edit_user = UserForm(request.POST, instance=request.user)
+        edit_form = ProfileForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if edit_user.is_valid() and edit_form.is_valid():
+            edit_user.save()
+            edit_form.save()
+            messages.success(request, "Your Profile has been updated!")
+            return redirect(reverse("profile", args=[username]))
+    else:
+        edit_user = UserForm(instance=request.user)
+        edit_form = ProfileForm(instance=request.user.profile)
+
+    context = {
+        "profile": profile,
+        "edit_user": edit_user,
+        "edit_form": edit_form,
+    }
+
+    return render(request, "profiles/edit_profile.html", context)
