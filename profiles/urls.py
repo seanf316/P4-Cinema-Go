a3f70d56bea33ed46cbe024174e2ca221fd85ddd
seanf316 <seanfinn316@gmail.com>
2023-02-24 18:51:27 +0000

    profiles urls updated with new view path

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
         views.profile,
         name="profile",
     ),
+    path(
+        "profile/<username>/edit_profile",
+        views.edit_profile,
+        name="edit_profile",
+    ),
 ]
