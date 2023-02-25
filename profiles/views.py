1f61f37dedecd455e18b4da09e437a75983ad832
seanf316 <seanfinn316@gmail.com>
2023-02-25 02:31:30 +0000

    Delete view created for CRUD functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 import requests
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 from .forms import ProfileForm, UserForm
 
 
+@login_required()
 def profile(request, username):
     user = get_object_or_404(User, username=username)
     profile = Profile.objects.get(user=user)
@@ -17,6 +20,7 @@ def profile(request, username):
     return render(request, "profiles/profile.html", context)
 
 
+@login_required()
 def edit_profile(request, username):
 
     user = get_object_or_404(User, username=username)
@@ -42,4 +46,19 @@ def edit_profile(request, username):
         "edit_form": edit_form,
     }
 
+    return render(request, "profiles/edit_profile.html", context)
+
+
+@login_required()
+def delete_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    if request.method == "POST":
+        logout(request)
+        user.delete()
+        messages.success(
+            request, "Sorry to see you go, your Account has been deleted."
+        )
+        return redirect(reverse("home"))
+
+    context = {"username": username}
     return render(request, "profiles/edit_profile.html", context)
