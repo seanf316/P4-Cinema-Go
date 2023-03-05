0fa5a6fa521d19299aa66dbcbae7ec49f9587153
seanf316 <seanfinn316@gmail.com>
2023-03-05 12:11:42 +0000

    Edit_Review view created

--- review/views.py
+++ review/views.py
@@ -59,3 +59,47 @@ def review(request, movie_id):
     }
 
     return render(request, "review/review.html", context)
+
+
+def edit_review(request, movie_id, review_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US&append_to_response=credits,videos,images"
+
+    response = requests.get(url)
+    movie_data = response.json()
+    backdrop = movie_data["backdrop_path"]
+    if backdrop:
+        hero = "https://image.tmdb.org/t/p/w1280/" + backdrop
+    else:
+        hero = "https://res.cloudinary.com/seanf316/image/upload/v1676857549/wp8923971_qd2bfr.jpg"
+
+    movie = Movie.objects.get(MovieId=movie_id)
+    review = Review.objects.get(id=review_id)
+    user = request.user
+
+    if review.user != user:
+        messages.error(request, "You are not authorized to edit this review.")
+        return redirect(reverse("review", args=[review.movie.MovieId]))
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.save()
+            messages.success(
+                request, f"{user.username} your review has been updated"
+            )
+
+            return redirect(reverse("moviedetails", args=[movie_id]))
+
+    else:
+        form = ReviewForm(instance=review)
+
+    context = {
+        "form": form,
+        "review": review,
+        "movie": movie,
+        "movie_data": movie_data,
+        "hero": hero,
+    }
+
+    return render(request, "review/review.html", context)
