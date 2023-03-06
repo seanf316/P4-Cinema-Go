20dc5591a09ef7c748d556c59f9563b0cd174d32
seanf316 <seanfinn316@gmail.com>
2023-03-06 23:35:53 +0000

    Created modal.js and placed in required templates

---
+++ static/javascript/modal.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById('exampleModal');
+var iframe = document.getElementById('trailer');
+
+modal.addEventListener('hide.bs.modal', function (event) {
+    if (event.target === modal) {
+        iframe.src = iframe.src;
+    }
+});
