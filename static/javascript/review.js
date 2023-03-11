dea1dce249180d8c390f68f765ab5378ac0971dd
seanf316 <seanfinn316@gmail.com>
2023-03-11 01:02:32 +0000

    Added js to Review card to reset scroll

---
+++ static/javascript/review.js
@@ -0,0 +1,10 @@
+let cards = document.querySelectorAll('.card');
+
+cards.forEach(function (card) {
+    card.addEventListener('mouseleave', function () {
+        card.querySelector('.card-body').scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
