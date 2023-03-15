21f0e63e777290dbcb08c9ba76db124ccc5a9975
seanf316 <seanfinn316@gmail.com>
2023-03-15 22:46:41 +0000

    js files updated to load after DOM

--- static/javascript/modal.js
+++ static/javascript/modal.js
@@ -1,8 +1,10 @@
-var modal = document.getElementById('exampleModal');
-var iframe = document.getElementById('trailer');
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('exampleModal');
+    var iframe = document.getElementById('trailer');
 
-modal.addEventListener('hide.bs.modal', function (event) {
-    if (event.target === modal) {
-        iframe.src = iframe.src;
-    }
+    modal.addEventListener('hide.bs.modal', function (event) {
+        if (event.target === modal) {
+            iframe.src = iframe.src;
+        }
+    });
 });
--- static/javascript/review.js
+++ static/javascript/review.js
@@ -1,10 +1,18 @@
-let cards = document.querySelectorAll('.card');
+document.addEventListener('DOMContentLoaded', function () {
+    let cards = document.querySelectorAll('.card');
 
-cards.forEach(function (card) {
-    card.addEventListener('mouseleave', function () {
-        card.querySelector('.card-body').scrollTo({
-            top: 0,
-            behavior: 'smooth'
+    cards.forEach(function (card) {
+        card.addEventListener('mouseleave', function () {
+            card.querySelector('.card-body').scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
+        card.addEventListener('touchend', function () {
+            card.querySelector('.card-body').scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         });
     });
 });
