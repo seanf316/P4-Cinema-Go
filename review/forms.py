917376be0d5ae619f88112422d5de6c91de29aef
seanf316 <seanfinn316@gmail.com>
2023-03-15 21:34:45 +0000

    Commentform created

--- review/forms.py
+++ review/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from djrichtextfield.widgets import RichTextWidget
-from review.models import Review
+from review.models import Review, Comment
 
 
 class ReviewForm(forms.ModelForm):
@@ -20,3 +20,23 @@ class ReviewForm(forms.ModelForm):
         ]
 
     review = forms.CharField(widget=RichTextWidget())
+
+
+class CommentForm(forms.ModelForm):
+    """
+    Form to Edit/Delete Review
+    """
+
+    class Meta:
+        """
+        Define model, form fields and widgets
+        """
+
+        model = Comment
+        fields = ("body",)
+
+        labels = {
+            "body": "Comment",
+        }
+
+        review = forms.CharField(widget=RichTextWidget())
