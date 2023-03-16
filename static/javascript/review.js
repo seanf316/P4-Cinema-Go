c753928c20e2cb499b9e59f758a2e390f1a97c4b
seanf316 <seanfinn316@gmail.com>
2023-03-16 01:18:21 +0000

    Added a delay to scroll effect

--- static/javascript/review.js
+++ static/javascript/review.js
@@ -2,11 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     let cards = document.querySelectorAll('.card');
 
     cards.forEach(function (card) {
+        let timeoutId;
+
         card.addEventListener('mouseleave', function () {
-            card.querySelector('.card-body').scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
+            clearTimeout(timeoutId);
+
+            timeoutId = setTimeout(function () {
+                card.querySelector('.card-body').scrollTo({
+                    top: 0,
+                    behavior: 'smooth'
+                });
+            }, 3000);
         });
     });
 });
