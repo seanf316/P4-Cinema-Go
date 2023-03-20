91976f2273a8df181fdac3a5b843354a4d12ec9e
seanf316 <seanfinn316@gmail.com>
2023-03-20 01:33:49 +0000

    Updated allauth settings

--- cinemago/settings.py
+++ cinemago/settings.py
@@ -184,4 +184,19 @@ DJRICHTEXTFIELD_CONFIG = {
     },
 }
 
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+# Account Setup
+
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+ACCOUNT_EMAIL_MAX_LENGTH = 100
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_EMAIL_VERIFICATION = "none"
